f3168d08a1a1ff39126c42752efba23225aca7d0
L Q <qinlihui@umich.edu>
2016-09-16 02:41:07 -0400

    Add search github db feature when user add new repo

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'uglifier', '>= 1.3.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+gem 'jquery-ui-sass-rails'
+
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
---
+++ app/assets/javascripts/repos.js
@@ -0,0 +1,54 @@
+//
+
+$(function() {
+	var getRepoData = function (request, response) {
+    	$.getJSON(
+            "https://api.github.com/search/repositories?q=" + request.term,
+        	function (data) {
+        		var repo_list = [];
+            	$.each(data.items, function(){
+            		repo_list.push(this.name);	
+            	});
+            	response(repo_list);
+            }
+        );
+    };
+	// Triggered when an item is selected form the manu
+	// action: replace the text field's value with the value of the selected item
+	var selectRepoItem = function (event, ui) {
+	    $("#repo_name").val(ui.item.value);
+	    return false;
+	};
+
+	$("#repo_name").autocomplete({
+	    source: getRepoData,
+	    select: selectRepoItem,
+	    minLength: 4
+	});
+
+	var getOwnerData = function (request, response) {
+    	$.getJSON(
+            "https://api.github.com/search/users?q=" + request.term,
+        	function (data) {
+        		var user_list = [];
+            	$.each(data.items, function(){
+					user_list.push(this.login);	
+            	});
+            	response(user_list);
+            }
+        );
+    };
+	// Triggered when an item is selected form the manu
+	// action: replace the text field's value with the value of the selected item
+	var selectOwnerItem = function (event, ui) {
+	    $("#repo_owner").val(ui.item.value);
+	    return false;
+	};
+
+	$("#repo_owner").autocomplete({
+	    source: getOwnerData,
+	    select: selectOwnerItem,
+	    minLength: 3
+	});
+});
+
